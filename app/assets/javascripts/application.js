c0a46e1f7ab7a7d857b498af59b30f94f65eafcf
Next <nextacademy@email.com>
2016-04-06 00:15:56 -0700

    basic bootstrap added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+<!-- Latest compiled and minified JavaScript -->
+// <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
